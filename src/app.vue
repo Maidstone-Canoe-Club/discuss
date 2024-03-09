15bc4492110c5ceb9e29c5bc60b76a02184c9fff
Matt <mattclegg93@gmail.com>
2024-03-09 08:47:28 +0000

    feat: initial commit

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<style>
+html {
+  font-family: 'Karla', Arial, sans-serif;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "Space Grotesk", Arial, sans-serif;
+}
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+﻿<script setup lang="ts">
+
+</script>
+
+<template>
+  <PageContent>
+    <div>index</div>
+  </PageContent>
+</template>
+
+<style scoped>
+
+</style>
